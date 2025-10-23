859cc07213c942167cde099584f16fc1078e9735
Sunho Cha <carprefer@postech.ac.kr>
2025-10-24 06:19:52 +0900

    [feat] apply mesh on attack

--- assn3_1/src/game/weapons/attack.h
+++ assn3_1/src/game/weapons/attack.h
@@ -13,7 +13,17 @@ public:
         glm::vec3 _axis = UP, 
         glm::vec3 _size = glm::vec3(1), 
         glm::vec3 _center = ZERO
-    ) : Object(_pos, _angle, _axis, _size, _center) {};
+    ) : Object(_pos, _angle, _axis, _size, _center) {
+
+        if (load_mesh("assets/rice.obj")) {
+            glm::mat4 meshTransform(1.0f);
+            meshTransform = glm::scale(meshTransform, glm::vec3(0.3f));
+            meshTransform = glm::rotate(meshTransform, glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+            meshTransform = glm::rotate(meshTransform, glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+            set_mesh_transform(meshTransform);
+            set_mesh_color(glm::vec4(0.0f, 1.0f, 0.0f, 1.0f));
+        }
+    };
 
     void draw_shape() const override;
     void update(float deltaTime);
